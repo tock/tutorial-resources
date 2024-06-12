52a71062d89de589c22c7d87c0fbbad99fa2f9b5
Tyler Potyondy <potyondyt@gmail.com>
2024-06-11 18:37:23 -0700

    Add build system

--- build_flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ot-nrf528xx
-./script/build nrf52840 UART_trans
-arm-none-eabi-objcopy -O ihex build/bin/ot-central-controller ot-central-controller.hex
-nrfjprog -f nrf52 --chiperase --program ot-central-controller.hex --reset --verify
-
---
+++ tutorial_app/build_flash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update submodule to correct submodule.
+git submodule update ../ot-nrf528xx --init --recursive 
+
+# In an effort to use submodules for OpenThread and the ot-nrf528xx
+# repos, we copy the app and an updated CMake file into the openthread
+# repo to utilize the default build system.
+cp -r central_controller_app ../ot-nrf528xx/openthread/examples/apps 
+cp CMakeLists.txt ../ot-nrf528xx/openthread/examples/apps
+
+# Invoke build script within ot-nrf528xx repo, convert to hex file,
+# and flash the file to the board.
+cd ot-nrf528xx
+./script/build nrf52840 UART_trans
+arm-none-eabi-objcopy -O ihex build/bin/ot-central-controller ot-central-controller.hex
+nrfjprog -f nrf52 --chiperase --program ot-central-controller.hex --reset --verify
+
